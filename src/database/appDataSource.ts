f377ee2b72664f96c573df46d64e8e5a5bae6ba8
thiegomoura <thi-ego@hotmail.com>
2023-07-09 19:46:58 -0400

    add env varibles to app and create docker file

--- src/database/appDataSource.ts
+++ src/database/appDataSource.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const MySQLDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "super@docker",
-  database: "CONVERTME",
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   synchronize: true,
   entities: ["./src/entities/*.ts"],
   migrations: ["./migrations/*.ts"],
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import routes from "./routes";
 
 dotenv.config()
 
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || 3000;
 
 const app = express();
 
